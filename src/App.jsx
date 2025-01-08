7a2d8f300ad78021977c9c86d0f38dec3ccf18a4
wilmmes <wilmmes@gmail.com>
2025-01-07 21:35:02 -0400

    practica1

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Diplomado en desarrollo Full Stack - Modulo 7 Front End React</h1>
+      <h1>Modulo 7 Front End React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
